b75664bb03f62eb1210126e5e6efd690402f5a62
Mi6u3l <miguelbragagomes@gmail.com>
2017-05-02 14:40:10 +0100

    css and js bundling

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var ngAnnotate = require('gulp-ng-annotate');
+var cleanCSS = require('gulp-clean-css');
+var concatCSS = require('gulp-concat');
+
+
+
+gulp.task('minifyJs', function (done) {
+    gulp.src([
+      'wwwroot/js/*.js',
+      'wwwroot/js/article/*.js',
+      'wwwroot/js/newsitem/*.js',
+      'wwwroot/js/homepage/*.js',
+      'wwwroot/js/network/*.js'
+    ])
+    .pipe(ngAnnotate())
+    .pipe(uglify())
+    .pipe(gulp.dest('./wwwroot/lib/_app'))
+
+
+});
+
+gulp.task('copyImg', function (done) {
+    gulp.src([
+        'wwwroot/img/*.png'
+    ])
+    .pipe(gulp.dest('./wwwroot/lib/img/'))
+});
+
+gulp.task('minifyCss', function (done) {
+    gulp.src([
+        'wwwroot/css/*.css'
+    ])
+    .pipe(cleanCSS())
+    .pipe(concatCSS('bundle.css'))
+    .pipe(gulp.dest('./wwwroot/lib/_app'))
+});
